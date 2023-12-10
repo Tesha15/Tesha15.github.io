e1ac484fcf14ae3de0deb5eff34d35ba75ab2861
Tesha15 <147662307+Tesha15@users.noreply.github.com>
2023-12-10 13:15:40 -0500

    Add files via upload

---
+++ cakes.js
@@ -0,0 +1,28 @@
+"use strict";
+
+
+
+window.addEventListener("load", function() {
+	var addButton = document.forms.addButton;
+	
+
+
+
+
+ 
+ 
+	
+
+    // Get product details
+    var productName = productContainer.querySelectorAll("h2, h3, h4, h5");
+    var productImage = productContainer.querySelectorAll("img");
+    var productPrice = productContainer.querySelectorAll("p");
+
+    // Create cart
+    var cartItem = {
+      name: productName,
+      image: productImage,
+      price: productPrice,
+     };
+  });
+}
